93bef075084777d5a1c660490f2e334efc5eb688
ezen-test <ezen-test@ezen-test-PC>
2019-06-28 15:54:27 +0900

    home.jsp에 수정했습니다~~ by 팀장 homeController 수정  by 팀장

--- sptest/src/main/java/com/ez/sptest/HomeController.java
+++ sptest/src/main/java/com/ez/sptest/HomeController.java
@@ -24,14 +24,15 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("Welcome home! 안녕하다  The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("serverTime", formattedDate );
+		//by 팀장님이다
+		model.addAttribute("serverTime123456789", formattedDate );
 		
 		return "home";
 	}
